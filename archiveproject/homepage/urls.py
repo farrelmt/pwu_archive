2fa2a4b15f7a6f7175e7738c640030245dc0b51e
IT PWU <it.pwujatim@gmail.com>
2025-11-24 14:32:50 +0700

    Add Basic Homepage

--- archiveproject/homepage/urls.py
+++ archiveproject/homepage/urls.py
@@ -4,5 +4,12 @@ from . import views
 app_name = 'homepage'
 
 urlpatterns = [
-    path('', views.index, name='homepage'),
+    path('', views.home, name='homepage'),
+    path('disposisi', views.disposisi, name='disposisi'),
+    path('notadinas', views.nota_dinas, name='notadinas'),
+    path('suratkeluar', views.surat_keluar, name='suratkeluar'),
+    path('monitor', views.monitoring, name='monitor'),
+    path('divisi', views.divisi, name='divisi'),
+    path('pengaturan', views.pengaturan, name='pengaturan'),
+    path('notif', views.notifikasi, name='notif'),
 ]
--- archiveproject/homepage/views.py
+++ archiveproject/homepage/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='accounts:login')
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+
+def disposisi(request):
+    return render(request, 'disposisi.html')
+
+def nota_dinas(request):
+    return render(request, 'notadinas.html')
+
+def surat_keluar(request):
+    return render(request, 'suratkeluar.html')
+
+def monitoring(request):
+    return render(request, 'monitoring.html')
+
+def divisi(request):
+    return render(request, 'divisi.html')
+
+def pengaturan(request):
+    return render(request, 'pengaturan.html')
+
+def notifikasi(request):
+    pass
