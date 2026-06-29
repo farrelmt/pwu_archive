5190f4e6fbe225be04ed0440d7f68b45aaadd060
IT PWU <it.pwujatim@gmail.com>
2026-06-29 10:55:35 +0700

    Bug fix docker settings

--- archiveproject/archiveproject/settings.py
+++ archiveproject/archiveproject/settings.py
@@ -15,8 +15,6 @@ DB_HOST = env('DB_HOST', default='db')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default='False')
 
-
-
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
 
 INSTALLED_APPS = [
